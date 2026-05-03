865c11d71ab99e920ea23b8071f821caba52d9d5
raidelo <raidelosix@gmail.com>
2026-05-02 22:20:48 -0400

    feat: add custom font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,15 @@
 import { Slot } from "expo-router";
 import { View, Text } from "react-native";
 
+import { useFonts } from "expo-font";
+
 const RootLayout = () => {
+  const [loaded] = useFonts({
+    JetBrainsMono: require("../assets/fonts/JetBrainsMonoNLNerdFontMono-Regular.ttf"),
+  });
+
+  if (!loaded) return null;
+
   return (
     <View>
       <Text>RootLayout</Text>
--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,9 @@ import { View, Text } from "react-native";
 const CalculatorApp = () => {
   return (
     <View>
-      <Text>CalculatorApp</Text>
+      <Text style={{ fontSize: 40, fontFamily: "JetBrainsMono" }}>
+        CalculatorApp
+      </Text>
     </View>
   );
 };
