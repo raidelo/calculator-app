ccdb302d804bba2c67d248834f3ef78fab54f382
raidelo <raidelosix@gmail.com>
2026-05-05 19:58:18 -0400

    feat: add simple functionality

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { View } from "react-native";
 
 import ButtonRow from "@/components/ButtonRow";
@@ -6,14 +8,47 @@ import ThemedText from "@/components/ThemedText";
 import { Colors } from "@/constants/theme";
 import { globalStyles } from "@/styles/global-styles";
 
+const DEFAULT_FORMULA_VALUE = "0";
+const DEFAULT_RESULT_VALUE = "";
+
 const CalculatorApp = () => {
+  const [formula, setFormula] = useState(DEFAULT_FORMULA_VALUE);
+  const [result, setResult] = useState(DEFAULT_RESULT_VALUE);
+
+  const buildFormula = (key: string) => {
+    if (formula === DEFAULT_FORMULA_VALUE) {
+      setFormula(key);
+    } else {
+      setFormula(formula + key);
+    }
+  };
+
+  const clearFormula = () => {
+    setFormula(DEFAULT_FORMULA_VALUE);
+  };
+
+  const deleteLastEntry = () => {
+    if (formula.length === 1) {
+      setFormula(DEFAULT_FORMULA_VALUE);
+    } else {
+      setFormula(formula.slice(0, formula.length - 1));
+    }
+  };
+
+  const equal = () => {
+    if (result !== DEFAULT_RESULT_VALUE) {
+      setFormula(result);
+      setResult(DEFAULT_RESULT_VALUE);
+    }
+  };
+
   return (
     <View style={globalStyles.calculatorContainer}>
       {/* Resultados */}
       <View style={globalStyles.textContainer}>
-        <ThemedText variant="main">50 x 50</ThemedText>
+        <ThemedText variant="main">{formula}</ThemedText>
 
-        <ThemedText variant="sub">2500</ThemedText>
+        <ThemedText variant="sub">{result}</ThemedText>
       </View>
 
       {/* Filas de botones */}
@@ -23,6 +58,9 @@ const CalculatorApp = () => {
             label="C"
             backgroundColor={Colors.lightGray}
             blackText
+            onPress={() => {
+              clearFormula();
+            }}
           />
           <CalculatorButton
             label="+/-"
@@ -33,35 +71,124 @@ const CalculatorApp = () => {
             label="del"
             backgroundColor={Colors.lightGray}
             blackText
+            onPress={() => {
+              deleteLastEntry();
+            }}
           />
-          <CalculatorButton label="÷" backgroundColor={Colors.orange} />
+          <CalculatorButton
+            label="÷"
+            backgroundColor={Colors.orange}
+            onPress={() => {
+              buildFormula("÷");
+            }}
+          />
         </ButtonRow>
 
         <ButtonRow>
-          <CalculatorButton label="7" />
-          <CalculatorButton label="8" />
-          <CalculatorButton label="9" />
-          <CalculatorButton label="x" backgroundColor={Colors.orange} />
+          <CalculatorButton
+            label="7"
+            onPress={() => {
+              buildFormula("7");
+            }}
+          />
+          <CalculatorButton
+            label="8"
+            onPress={() => {
+              buildFormula("8");
+            }}
+          />
+          <CalculatorButton
+            label="9"
+            onPress={() => {
+              buildFormula("9");
+            }}
+          />
+          <CalculatorButton
+            label="x"
+            backgroundColor={Colors.orange}
+            onPress={() => {
+              buildFormula("x");
+            }}
+          />
         </ButtonRow>
 
         <ButtonRow>
-          <CalculatorButton label="4" />
-          <CalculatorButton label="5" />
-          <CalculatorButton label="6" />
-          <CalculatorButton label="-" backgroundColor={Colors.orange} />
+          <CalculatorButton
+            label="4"
+            onPress={() => {
+              buildFormula("4");
+            }}
+          />
+          <CalculatorButton
+            label="5"
+            onPress={() => {
+              buildFormula("5");
+            }}
+          />
+          <CalculatorButton
+            label="6"
+            onPress={() => {
+              buildFormula("6");
+            }}
+          />
+          <CalculatorButton
+            label="-"
+            backgroundColor={Colors.orange}
+            onPress={() => {
+              buildFormula("-");
+            }}
+          />
         </ButtonRow>
 
         <ButtonRow>
-          <CalculatorButton label="1" />
-          <CalculatorButton label="2" />
-          <CalculatorButton label="3" />
-          <CalculatorButton label="+" backgroundColor={Colors.orange} />
+          <CalculatorButton
+            label="1"
+            onPress={() => {
+              buildFormula("1");
+            }}
+          />
+          <CalculatorButton
+            label="2"
+            onPress={() => {
+              buildFormula("2");
+            }}
+          />
+          <CalculatorButton
+            label="3"
+            onPress={() => {
+              buildFormula("3");
+            }}
+          />
+          <CalculatorButton
+            label="+"
+            backgroundColor={Colors.orange}
+            onPress={() => {
+              buildFormula("+");
+            }}
+          />
         </ButtonRow>
 
         <ButtonRow>
-          <CalculatorButton label="0" size={2} />
-          <CalculatorButton label="." />
-          <CalculatorButton label="=" backgroundColor={Colors.orange} />
+          <CalculatorButton
+            label="0"
+            size={2}
+            onPress={() => {
+              buildFormula("0");
+            }}
+          />
+          <CalculatorButton
+            label="."
+            onPress={() => {
+              buildFormula(".");
+            }}
+          />
+          <CalculatorButton
+            label="="
+            backgroundColor={Colors.orange}
+            onPress={() => {
+              equal();
+            }}
+          />
         </ButtonRow>
       </View>
     </View>
